153e7228bd421143072674c137abe90507fda9e1
Thiago Moretto <thiago.moretto@gmail.com>
2011-06-29 14:55:44 -0400

    integracao com o flurry e com o xtify

--- src/br/com/jera/Main.java
+++ src/br/com/jera/Main.java
@@ -1,8 +1,11 @@
 package br.com.jera;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import br.com.jeramobstats.JeraAgent;
+
 import com.phonegap.DroidGap;
+import com.xtify.android.sdk.PersistentLocationManager;
 
 public class Main extends DroidGap {
     /** Called when the activity is first created. */
@@ -10,5 +13,39 @@ public class Main extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/template.html");
+
+        // xtify specific-code
+        Context context = this;
+        final PersistentLocationManager persistentLocationManager = new PersistentLocationManager(context);
+        Thread xtifyThread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                persistentLocationManager.setNotificationIcon(R.drawable.notification);
+                persistentLocationManager.setNotificationDetailsIcon(R.drawable.icon);
+                boolean trackLocation = persistentLocationManager.isTrackingLocation();
+                boolean deliverNotifications = persistentLocationManager.isDeliveringNotifications();
+                if (trackLocation || deliverNotifications)
+                {
+                    persistentLocationManager.startService();
+                }
+            }
+        });
+        xtifyThread.start(); // to avoid Android's application-not-responding dialog box,
+                             // do non-essential work in another thread
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        JeraAgent.onStartSession(this, "YI885C7NWJ5Y37T8ESMU");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        JeraAgent.onEndSession(this);
     }
 }
