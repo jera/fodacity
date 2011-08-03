3fa7344043c478b6c2aa2b47d25f082f595b37d3
JEFFMOR <jeffmor@gmail.com>
2011-08-03 19:51:50 -0400

    Retirando push notification

--- src/br/com/jera/soufodatube/Main.java
+++ src/br/com/jera/soufodatube/Main.java
@@ -1,55 +1,32 @@
 package br.com.jera.soufodatube;
 
-import android.content.Context;
 import android.os.Bundle;
 import br.com.jeramobstats.JeraAgent;
 
 import com.phonegap.DroidGap;
-import com.xtify.android.sdk.PersistentLocationManager;
 
 public class Main extends DroidGap {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/template.html");
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/template.html");
 		SplashDialog dialog = new SplashDialog(this);
 		if (!dialog.isSplashed()) {
 			dialog.show();
 			dialog.setSplashed(true);
 		}
-        // xtify specific-code
-        Context context = this;
-        final PersistentLocationManager persistentLocationManager = new PersistentLocationManager(context);
-        Thread xtifyThread = new Thread(new Runnable() {
-            @Override
-            public void run()
-            {
-                persistentLocationManager.setNotificationIcon(R.drawable.notification);
-                persistentLocationManager.setNotificationDetailsIcon(R.drawable.icon);
-                boolean trackLocation = persistentLocationManager.isTrackingLocation();
-                boolean deliverNotifications = persistentLocationManager.isDeliveringNotifications();
-                if (trackLocation || deliverNotifications)
-                {
-                    persistentLocationManager.startService();
-                }
-            }
-        });
-        xtifyThread.start(); // to avoid Android's application-not-responding dialog box,
-                             // do non-essential work in another thread
-    }
+	}
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        JeraAgent.onStartSession(this, "YI885C7NWJ5Y37T8ESMU");
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		JeraAgent.onStartSession(this, "YI885C7NWJ5Y37T8ESMU");
+	}
 
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        JeraAgent.onEndSession(this);
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		JeraAgent.onEndSession(this);
+	}
 }
